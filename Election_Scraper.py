3d0ca3bf67d31641ec79eb5055ce07acc61d0816
Josef Novotny <josef.novotnyy@gmail.com>
2022-02-13 11:16:54 +0100

    v1.1

--- Election_Scraper.py
+++ Election_Scraper.py
@@ -1,15 +1,16 @@
 import csv
 import sys
+import time
 import requests
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 
 
-def request_bs4(url):
+def request_bs4(url: str) -> BeautifulSoup:
     return BeautifulSoup(requests.get(url).text, "html.parser")
 
 
-def check_args():
+def check_args() -> bool:
     if len(sys.argv) != 3:
         print("Two arguments expected, can't run scraper.")
         return False
@@ -23,23 +24,32 @@ def check_args():
         return True
 
 
-def town_details(soup, url):
+def get_town_codes(soup: BeautifulSoup) -> list:
     """
     filtering data from soup str
-    returns lists of direct urls
-    and codes of all towns
+    returns lists of town direct urls
     """
-    base_url = url.split("ps3")[0]
-    urls = []
     codes = []
     for x in soup.find_all("td", class_="cislo"):
         for a in x.find_all("a"):
-            urls.append(urljoin(base_url, a.get("href")))
             codes.append(int(a.get_text()))
-    return urls, codes
+    return codes
 
 
-def get_political_parties(soup):
+def get_town_urls(soup: BeautifulSoup, url: str) -> list:
+    """
+    filtering data from soup str
+    returns lists of town codes
+    """
+    base_url = url.split("ps3")[0]
+    urls = []
+    for x in soup.find_all("td", class_="cislo"):
+        for a in x.find_all("a"):
+            urls.append(urljoin(base_url, a.get("href")))
+    return urls
+
+
+def get_political_parties(soup: BeautifulSoup) -> list:
     """
     filtering data from soup str
     returns list of all parties
@@ -54,7 +64,7 @@ def get_political_parties(soup):
     return parties
 
 
-def votes_to_int(lists_in_list):
+def votes_to_int(lists_in_list: list) -> list:
     """
     changing scraped votes data
     from str to int
@@ -64,10 +74,10 @@ def votes_to_int(lists_in_list):
     return lists_in_list
 
 
-def write_to_csv(file, header, details):
+def write_to_csv(file: str, header: list, details: list) -> csv:
     """
     csv writing function, 3 arguments required
-    > file name
+    > desired file name
     > list with header and parties names
     > list of scraped election data
     """
@@ -80,17 +90,16 @@ def write_to_csv(file, header, details):
 def scraper():
     # if not check_args():
     #     quit()
-    # # default_url = sys.argv[1]
-    # # file_name = sys.argv[2]
+    # default_url = sys.argv[1]
+    # file_name = sys.argv[2]
     default_url = "https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=2&xnumnuts=2102"
     file_name = "vysledky_beroun.csv"
     print(f"Downloading data from selected URL: {default_url}")
     print(f"Saving data in file: {file_name}")
-    print("Quitting Election_Scraper...")
 
     soup_1 = request_bs4(default_url)
-    urls = town_details(soup_1, default_url)[0]
-    codes = town_details(soup_1, default_url)[1]
+    codes = get_town_codes(soup_1)
+    urls = get_town_urls(soup_1, default_url)
 
     soup_2 = request_bs4(urls[0])
 
@@ -115,8 +124,10 @@ def scraper():
             a += 1
         rows.append(row)
     csv_rows = votes_to_int(rows)
+
     write_to_csv(file_name, csv_header, csv_rows)
 
 
 if __name__ == "__main__":
     scraper()
+    print("Quitting Election_Scraper...")
